074e2a32cd7cd392e9a5dca66e3e968ed31807b4
Dzianis Vasileuski <d.vasileuski@gmail.com>
2017-10-05 08:59:25 +0300

    Minor changes

--- src/main/java/by/vasilevsky/leasing/dao/BaseDao.java
+++ src/main/java/by/vasilevsky/leasing/dao/BaseDao.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class BaseDao {
+public abstract class BaseDao {
 	protected void closeResources(ResultSet rs, Statement stmt, Connection con) {
 		try {
 			if (rs != null) {
